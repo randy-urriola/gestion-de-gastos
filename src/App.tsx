312d47a25f135e947f542b4593c77772df84faf4
randy-urriola <randy.urriolah@gmail.com>
2026-09-09 21:21:52 -0500

    Agregando las funcionalidades del reduce: agregar presupuesto, mostrar la ventana modal para registrar gastos y cerrar la ventana + todos los estilos de la ventana junto a la dependencia DatePicker para el calendario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useMemo } from 'react';
 import BudgetForm from './components/BudgetForm';
 import { useBudget } from './hooks/useBudget';
+import BudgetTracker from './components/BudgetTracker';
+import ExpenseModal from './components/ExpenseModal';
 
 function App() {
-	const { state, dispatch } = useBudget();
-	console.log(state);
+	const { state } = useBudget();
+	console.log(state.budget);
+
+	const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
 
 	return (
 		<>
@@ -14,8 +19,14 @@ function App() {
 			</header>
 
 			<div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-				<BudgetForm />
+				{isValidBudget ? <BudgetTracker /> : <BudgetForm />}
 			</div>
+
+			{isValidBudget && (
+				<main className="max-w-3xl mx-auto py-10">
+					<ExpenseModal />
+				</main>
+			)}
 		</>
 	);
 }
