718b94d9873223344cb156886b44d079edb7640a
randy-urriola <randy.urriolah@gmail.com>
2026-09-15 21:32:31 -0500

    Mostrando los gastos y los iconos de las categorías + agregando la interacción visual Swipe (swipeable list) de actualizar o eliminar un gasto

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from './components/BudgetForm';
 import { useBudget } from './hooks/useBudget';
 import BudgetTracker from './components/BudgetTracker';
 import ExpenseModal from './components/ExpenseModal';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
 	const { state } = useBudget();
@@ -23,6 +24,7 @@ function App() {
 
 			{isValidBudget && (
 				<main className="max-w-3xl mx-auto py-10">
+					<ExpenseList />
 					<ExpenseModal />
 				</main>
 			)}
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	optimizeDeps: {
+		include: ['prop-types'], // 👈 aquí forzamos que Vite/Rolldown lo trate como dependencia ESM
+	},
 });
